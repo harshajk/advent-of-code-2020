7ab598cee2e3e0d6cf53d9f670704d0952130274
Harsha J K <harshajk41@gmail.com>
2020-12-08 23:59:04 +0530

    Add placeholders for day 9

---
+++ day09.py
@@ -0,0 +1,25 @@
+#import modules here
+
+# File input here
+filename = "input/day09test.txt"
+
+#Globals here
+
+
+#Code here
+# Input file parsing:
+with open(filename,"r") as file:
+    for line in file.readlines():
+        line = line.strip()
+        val = line.split()
+
+def day08_p1():
+    pass
+
+def day08_p2():    
+    pass
+
+print("\nPart-1") 
+print(day08_p1()) #
+print("\nPart-2")
+print(day08_p2()) #
